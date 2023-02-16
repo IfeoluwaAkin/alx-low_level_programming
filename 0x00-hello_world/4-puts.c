454de39a844ffab98fd96f77b84028af0c46d08e
IfeoluwaAkin <oluwakayikun2010@gmail.com>
2023-02-16 10:59:53 -0800

    Correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main- Use puts to print a string 
- * Return: Always 0 (Success)
+ * main- use puts to print a string 
+ * Return: 0
  */
 
 int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
