331ad447e8eab03113e0601d1f76bcf8e0443f72
IfeoluwaAkin <oluwakayikun2010@gmail.com>
2023-02-17 04:06:31 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints string and standard error
+ * main - prints a new line followed by standard error
  * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - 
-	Dora Korpar,2015-10-19\n",59);
+	fputs("and that piece of art is useful\" - Dora Korpar,2015-10-19\n",stdout);
 	return (1);
 }
