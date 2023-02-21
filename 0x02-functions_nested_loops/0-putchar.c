6f1fc24b23f056532dfa9da04fc6f978251a5bb4
IfeoluwaAkin <oluwakayikun2010@gmail.com>
2023-02-21 09:24:40 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,18 @@
-#include <unistd.h>
+#include "main.h"
 /**
  * main- main function
  * Return: 0
  */
-int _putchar(char n)
+int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
