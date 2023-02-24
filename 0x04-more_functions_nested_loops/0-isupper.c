3b700a0b25092a8041c056fd57c13e0095be48ea
IfeoluwaAkin <oluwakayikun2010@gmail.com>
2023-02-24 09:06:20 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check if the character is uppercase.
+ * @c: input character.
+ * Return: If it an uppercase character return 1, others 0.
+ */
+int _isupper(int c);
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
